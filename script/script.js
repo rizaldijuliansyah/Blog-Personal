bcb9896ec4b11b3b3a03e4836c8cebc4eac5a849
Rizaldi Juliansyah <rizaldijuliansyah12@gmail.com>
2023-03-01 16:37:24 +0700

    Add file

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+});
+
+mediumZoom('.zoom',{
+    margin: 50
+})
